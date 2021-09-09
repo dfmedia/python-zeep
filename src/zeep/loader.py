df9df4b5028fa3083ce79675b49958bedeeb9772
Richard Haussmann <rhaussmann@medianewsgroup.com>
2021-09-09 15:59:44 -0600

    switched to content.content

--- src/zeep/loader.py
+++ src/zeep/loader.py
@@ -39,8 +39,8 @@ def parse_xml(content: str, transport, base_url=None, settings=None):
     :rtype: lxml.etree._Element
 
     """
-    print(content)
-    content = content.decode('utf-8')
+    content = content.content
+
     settings = settings or Settings()
     recover = not settings.strict
     parser = XMLParser(
